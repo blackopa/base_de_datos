3d03e05319308300b411401568c35eb41fd1d3b8
Joaquin Morales <jrmoralesm97@gmail.com>
2018-06-28 17:35:43 -0400

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -43,8 +43,17 @@ insert into Clases (nombre) values('Barbaro', 'Bardo', 'Brujo', 'Clerigo', 'Drui
 insert into Clases (dado_golpe) values('d12', 'd8', 'd8', 'd8', 'd8', 'd10', 'd10', 'd6', 'd6', 'd8', 'd10', 'd8')
 insert into Clases (atributo_adepto) values('Fuerza', 'Carisma', 'Carisma', 'Sabiduria', 'Sabiduria', 'Destreza y Sabiduria', 'Fuerza o Destreza', 'Carisma', 'Inteligencia', 'Destreza y Sabiduria', 'Fuerza y Carisma', 'Destreza')
 
+insert into Objetos (nombre) values('Baston', 'Daga', Garrote', 'Garrote grande', 'Hacha de mano', 'Hoz', 'Jabalina', 'Lanza', 'Martillo ligero', 'Maza')
+insert into Objetos (descripcion) values('1d6 Contudente', '1d4 Perforante', '1d4 Contundente', '1d8 Contundente', '1d6 Cortante', '1d4 Cortante', '1d6 Perforante', '1d6 Perforente', '1d4 Contundente', '1d6 Contundente') 
 
+insert into Objetos (nombre) values('Arco corto', 'Ballesta ligera', 'Dardo', 'Honda')
+insert into Objetos (descripcion) values('1d6 Perforante', '1d8 Perforante', '1d4 Perforante', '1d4 Contundente')
 
+insert into Objetos (nombre) values('Alabarda', 'Cimitarra', 'Espada corta', 'Espada larga', 'Espadon', 'Estoque', 'Flagelo', 'Guja', 'Hacha a dos manos', 'Hacha de guerra', 'Lanza de caballeria', 'Latigo', 'Lucero del alba', 'Martillo de guerra', 'Maza a dos manos', 'Pica', 'Pico de guerra', 'Tridente')
+insert into Objetos (descripcion) values('1d10 Cortante ¡La mejor arma de todas!', '1d6 Cortante', '1d6 Perforante', '1d8 Cortante', '2d6 Cortante', '1d8 Perforante', '1d8 Contudente', '1d10 Cortante', '1d12 Cortante', '1d8 Cortante', '1d12 Perforante', '1d4 Cortante', '1d8 Perforante', '1d8 Contundente', '2d6 Contundente', '1d10 Perforante', '1d8 Perforante', '1d6 Perforante')  
+
+insert into Objetos (nombre) values('Arco largo', 'Ballesta de mano', 'Ballesta pesada', 'cerbatana', 'Red')
+insert into Objetos (descripcion) values('1d8 Perforante', '1d6 Perforante', '1d10 Perforante', '1 Perforante', '¿De verdad crees que esto hace daño?')
 
 """
 cur.execute(sql)
